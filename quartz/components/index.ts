b380cae0d0974158f0417205dc5ee3d253e9cd54
chereny <2392036@ewha.ac.kr>
2025-07-04 15:11:21 +0900

    add comment4

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -3,13 +3,27 @@ import { QuartzComponent } from "./types"
 // @ts-ignore
 import ArticleTitle from "./ArticleTitle"
 // @ts-ignore
+import Backlinks from "./Backlinks"
+// @ts-ignore
 import Breadcrumbs from "./Breadcrumbs"
+// @ts-ignore
+import Comments from "./Comments"
+// @ts-ignore
+import ConditionalRender from "./ConditionalRender"
+// @ts-ignore
+import Content from "./Content"
 // @ts-ignore
 import ContentMeta from "./ContentMeta"
 // @ts-ignore
 import Darkmode from "./Darkmode"
 // @ts-ignore
+import DesktopOnly from "./DesktopOnly"
+// @ts-ignore
 import Explorer from "./Explorer"
+// @ts-ignore
+import Flex from "./Flex"
+// @ts-ignore
+import FolderContent from "./FolderContent"
 // @ts-ignore
 import Footer from "./Footer"
 // @ts-ignore
@@ -17,37 +31,47 @@ import Graph from "./Graph"
 // @ts-ignore
 import Head from "./Head"
 // @ts-ignore
+import MobileOnly from "./MobileOnly"
+// @ts-ignore
+import NotFound from "./NotFound"
+// @ts-ignore
 import PageTitle from "./PageTitle"
 // @ts-ignore
 import ReaderMode from "./ReaderMode"
 // @ts-ignore
 import Search from "./Search"
 // @ts-ignore
+import Spacer from "./Spacer"
+// @ts-ignore
+import TagContent from "./TagContent"
+// @ts-ignore
 import TagList from "./TagList"
 // @ts-ignore
 import TableOfContents from "./TableOfContents"
-// @ts-ignore
-import Backlinks from "./Backlinks"
-// @ts-ignore
-import Comments from "./Comments"
 
 export {
   ArticleTitle,
+  Backlinks,
   Breadcrumbs,
+  Comments,
+  ConditionalRender,
+  Content,
   ContentMeta,
   Darkmode,
+  DesktopOnly,
   Explorer,
+  Flex,
+  FolderContent,
   Footer,
   Graph,
   Head,
+  MobileOnly,
+  NotFound,
   PageTitle,
   ReaderMode,
   Search,
+  Spacer,
+  TagContent,
   TagList,
   TableOfContents,
-  Backlinks,
-  Comments,
 }
-
-// 여기서 중복 export 제거 - Comments는 위에서 이미 export했으므로 아래 줄은 삭제
-// export { default as Comments } from "./Comments"  // 이 줄 삭제
